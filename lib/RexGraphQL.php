a2c9939ca0cbe8b1b2b96fc00179317bc2536529
Daniel Planötscher <d.planoetscher@kreatif.it>
2023-08-10 17:28:53 +0200

    bugfix null check

--- lib/RexGraphQL.php
+++ lib/RexGraphQL.php
@@ -17,12 +17,14 @@ class RexGraphQL
     {
         return self::getMode() == self::ENDPOINT_MODE_KEY || self::DEFAULT_MODE == self::ENDPOINT_MODE_KEY;
     }
+
     public static function isHeadlessMode(): bool
     {
         return self::getMode() == self::HEADLESS_MODE_KEY || self::DEFAULT_MODE == self::HEADLESS_MODE_KEY;
     }
 
-    public static function getMode(): string {
+    public static function getMode(): ?string
+    {
         return \rex_addon::get('graphql')->getConfig(self::MODE_CONFIG_KEY);
     }
 }
